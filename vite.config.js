7d81b71876a20ea9fc136e30ec996ae89197f3a7
yuncai.chen <yuncai.chen@bitboxonline.com>
2023-10-27 13:19:40 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://yuncai.chen.github.io/React-Proyect-Yarn',
+  base: 'https://yuncaich.github.io/React-Proyect-Yarn',
 })
